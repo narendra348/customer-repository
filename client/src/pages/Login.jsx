8b52fb50c65a84453e75103f9a5f227143e5ef4f
narendra348 <tallapureddynarendrakumarreddy@gmail.com>
2026-07-20 08:36:33 +0530

    Fixed login API URL

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "https://https://customer-repository.onrender.com/api/auth/login",
+        "https://customer-repository.onrender.com/api/auth/login",
         form
       );
 
